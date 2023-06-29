c35e2a2ceb7662fe1938536ba4098a7383b502ae
CarlosBarquin <116747942+CarlosBarquin@users.noreply.github.com>
2023-06-29 18:04:22 +0200

    Add files via upload

---
+++ src/main.ts
@@ -0,0 +1,38 @@
+import { Server } from "std/http/server.ts";
+import { GraphQLHTTP } from "gql";
+import { makeExecutableSchema } from "graphql_tools";
+
+import { typeDefs } from "./schema.ts";
+import { Mutation } from "./resolvers/mutation.ts";
+import { Query } from "./resolvers/query.ts";
+import bookResolver from "./resolvers/Book.ts";
+import autorResolver from "./resolvers/autor.ts";
+import editorialResolver from "./resolvers/editoral.ts";
+
+const resolvers = {
+  Query,
+  Mutation,
+  Book : bookResolver,
+  Author : autorResolver,
+  PressHouse : editorialResolver
+};
+
+const p = Deno.env.get("PORT") as string
+
+const s = new Server({
+  handler: async (req) => {
+    const { pathname } = new URL(req.url);
+
+    return pathname === "/graphql"
+      ? await GraphQLHTTP<Request>({
+          schema: makeExecutableSchema({ resolvers, typeDefs }),
+          graphiql: true,
+        })(req)
+      : new Response("Not Found", { status: 404 });
+  },
+  port: parseInt(p),
+});
+
+s.listenAndServe();
+
+console.log(`Server running on: http://localhost:3000/graphql`);
---
+++ src/types.ts
@@ -0,0 +1,11 @@
+
+export type autor= {
+  name : string
+  books : libro[]
+}
+
+export type libro = {
+  id : string
+  title : string
+  pages : number
+}
